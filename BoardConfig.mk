1f81d21074efddc3757613c0c77229cdb0277eaf
falk0069 <falk0069@gmail.com>
2024-03-24 12:37:35 -0500

    addded fireware and logging for touch not working

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,9 +75,12 @@ PLATFORM_VERSION := 16.1.0
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
+#TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 #TW_USE_TOOLBOX := true
+
+#add logging for touch not working
+TWRP_EVENT_LOGGING := true
 
 #Extra
 BOARD_HAS_MTK_HARDWARE := true
