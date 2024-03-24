cc33e09a5be058fdcbbeb7955a53af26dedda568
falk0069 <falk0069@gmail.com>
2024-03-24 06:46:08 -0500

    going back to previous BoardConfig and disabling as much as possible due to size.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,80 +1,96 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
+#CODENAME=tb8167p3_bsp
+#VENDORNAME=alps
 
-DEVICE_PATH := device/alps/tb8167p3_bsp
+LOCAL_PATH := device/alps/tb8167p3_bsp
 
-# For building with minimal manifest
-ALLOW_MISSING_DEPENDENCIES := true
+TARGET_BOARD_PLATFORM := mt8167               # From ro.mediatek.platform, but lowercase value
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := tb8167p3_bsp     # From ro.product.board
+
+# These two are for MTK Chipsets only
+BOARD_USES_MTK_HARDWARE := true
+BOARD_HAS_MTK_HARDWARE := true
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true            # To add info about F2FS Filesystem Data Block
+# Put The Size of your Recovery Partition below, get it from your "MT****_Android_scatter.txt"
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+# BOARD_USES_FULL_RECOVERY_IMAGE := true      # Uncomment this line if you want to remove size restriction
+BOARD_FLASH_BLOCK_SIZE := 0                   # Might be different for your chip
+BOARD_HAS_NO_REAL_SDCARD := true              # Depricated
+# BOARD_HAS_NO_SELECT_BUTTON := true          # Depricated
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true           # Delete if your partition table has /misc
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
+RECOVERY_SDCARD_ON_DATA := true               # Optional: If /sdcard partition is emulated on /data partition 
+
+# TWRP stuff
+TW_EXCLUDE_SUPERSU := true                    # true/false: Add SuperSU or not
+TW_INCLUDE_CRYPTO := false                     # true/false: Add Data Encryption Support or not
+TW_INPUT_BLACKLIST := "hbtp_vm"               # Optional: Disables virtual mouse
+TW_SCREEN_BLANK_ON_BOOT := true
+TW_THEME := portrait_hdpi                     # Set the exact theme you wanna use. If resulation doesn't match, define the height/width
+DEVICE_RESOLUTION := 720x1280                 # The Resolution of your Device
+TARGET_SCREEN_HEIGHT := 1280                    # The height
+TARGET_SCREEN_WIDTH := 720                      # The width
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
+# Set the Brightness Control File Path below (as per your chip/device)
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+# Set the Path of Logical Units (LUNs) for Storage below (as per your chip/device)
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0.auto/gadget/lun%d/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 80                   # Set custom brightness, low is better
+
+TW_INCLUDE_NTFS_3G := false                    # Include NTFS Filesystem Support
+TW_INCLUDE_FUSE_EXFAT := false                 # Include Fuse-ExFAT Filesystem Support
+TWRP_INCLUDE_LOGCAT := false                   # Include LogCat Binary
+TW_INCLUDE_FB2PNG := false                     # Include Screenshot Support
+TW_DEFAULT_LANGUAGE := en                     # Set Default Language 
+TW_EXTRA_LANGUAGES := false
+
+# Kernel
+TARGET_IS_64_BIT := false                      # true/false: Determine if the device is 64-bit or not
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+TARGET_PREBUILT_RECOVERY_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+# Get the CMDLine, Base, Pagesize and offsets from Unpacked recovery image and put below
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
+
+# Set FSTAB
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/root/etc/recovery.fstab
+
+TARGET_BOARD_SUFFIX := _64                    # Remove if the device is 32-bit
+TARGET_USES_64_BIT_BINDER := true             # Remove if the device is 32-bit
 
 # Architecture
+# According to the device's architecture (64-bit or 32-bit)
+ifeq ($(TARGET_IS_64_BIT),true)
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53             # Change the value to "generic" if build fails suddenly due to arch error
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53         # Change the value to "generic" if build fails suddenly due to arch error
+TARGET_CPU_ABI_LIST_64_BIT := $(TARGET_CPU_ABI)
+TARGET_CPU_ABI_LIST_32_BIT := $(TARGET_2ND_CPU_ABI),$(TARGET_2ND_CPU_ABI2)
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI_LIST_64_BIT),$(TARGET_CPU_ABI_LIST_32_BIT)
+else
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := generic
-
-TARGET_USES_64_BIT_BINDER := true
-
-# APEX
-OVERRIDE_TARGET_FLATTEN_APEX := true
-
-# Bootloader
-TARGET_NO_BOOTLOADER := true
-
-# Display
-TARGET_SCREEN_DENSITY := 160
-
-# Kernel
-BOARD_KERNEL_BASE := 0x40000000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,32N2 buildvariant=user
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_RAMDISK_OFFSET := 0x15000000
-BOARD_KERNEL_TAGS_OFFSET := 0x14000000
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_KERNEL_CONFIG := tb8167p3_bsp_defconfig
-TARGET_KERNEL_SOURCE := kernel/alps/tb8167p3_bsp
-
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := true
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+TARGET_CPU_VARIANT := cortex-a7              # Change the value to "generic" if build fails suddenly due to arch error
+TARGET_CPU_ABI_LIST := $(TARGET_CPU_ABI),$(TARGET_CPU_ABI2)
 endif
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
-BOARD_BOOTIMAGE_PARTITION_SIZE := 11436032
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 11436032
-BOARD_HAS_LARGE_FILESYSTEM := true
-BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
-BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-
-# Platform
-TARGET_BOARD_PLATFORM := mt8167
-
-# Recovery
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-# Security patch level
-VENDOR_SECURITY_PATCH := 2021-08-01
-
-# Hack: prevent anti rollback
-PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
-PLATFORM_VERSION := 16.1.0
-
-# TWRP Configuration
-TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_USE_TOOLBOX := true
